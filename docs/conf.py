f4f35f2ae7523cf4a041ed1be66793205f17215c
Kai Welke <17420240+kai687@users.noreply.github.com>
2020-05-16 17:50:49 +0200

    cleaned up conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,15 +8,13 @@ author = "Kai Welke"
 
 extensions = []
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["public"]
 nitpicky = True
 default_role = "literal"
 
 # -- Options for HTML output -------------------------------------------------
 
+html_title = "Sphinxawesome Theme"
 html_theme = "sphinxawesome_theme"
 html_theme_path = ["../"]
 html_theme_options = {"nav_include_hidden": True}
