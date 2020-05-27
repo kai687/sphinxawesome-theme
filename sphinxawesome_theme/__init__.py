290e7b242016d3ad975a4da60ee148b2e52d1e10
Kai Welke <kai687@pm.me>
2020-05-27 11:23:18 +0200

    version bump

--- sphinxawesome_theme/__init__.py
+++ sphinxawesome_theme/__init__.py
@@ -10,7 +10,7 @@ from os import path
 from typing import Dict, Any
 from sphinx.application import Sphinx
 
-__version__ = "1.1.0"
+__version__ = "1.3.0"
 
 def setup(app: Sphinx) -> Dict[str, Any]:
     """register this theme with Sphinx"""
