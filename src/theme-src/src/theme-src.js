188bbc7c1deadc97c81ef87c858652bd5f2194ac
Kai Welke <kai687@pm.me>
2020-07-05 13:57:16 +0200

    fix: minor accessibility tweaks

--- src/theme-src/src/theme-src.js
+++ src/theme-src/src/theme-src.js
@@ -128,7 +128,7 @@ setTimeout(() => {
       _("Clear highlighted words") +
       "</a>";
     snackbar.classList.remove("bg-gray-900", "text-gray-100");
-    snackbar.classList.add("bg-gray-200", "text-blue-600");
+    snackbar.classList.add("bg-gray-200", "text-blue-700");
     snackbar.style.opacity = 1;
     snackbar.style.transform = "translate(0,0)";
 
@@ -163,7 +163,7 @@ function showSnackbar(message) {
 function hideSnackbar() {
   snackbar.style.opacity = 0;
   snackbar.style.transform = "translate(0,100%)";
-  snackbar.classList.remove("bg-gray-200", "text-blue-600");
+  snackbar.classList.remove("bg-gray-200", "text-blue-700");
   snackbar.classList.add("bg-gray-900", "text-gray-100");
 }
 
