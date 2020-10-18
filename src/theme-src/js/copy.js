e073a8586baf40192a5023028dba9fbd88c71989
Kai Welke <kai687@pm.me>
2020-10-18 07:43:27 +0200

    full css tooltip implementation from primer/css

--- src/theme-src/js/copy.js
+++ src/theme-src/js/copy.js
@@ -1,5 +1,14 @@
-import { showSnackbar } from "./snackbar";
 import ClipboardJS from "clipboard";
+
+function showSuccessTooltip (event) {
+  // change the tooltip message on successful copy event
+  const btn = event.trigger;
+  const before = btn.getAttribute("aria-label");
+  btn.setAttribute("aria-label", "Copied!");
+  setTimeout(() => {
+    btn.setAttribute("aria-label", before);
+  }, 2500);
+}
 
 function copyEvents () {
   // Use clipboard.js for the headerlinks. Copy the href.
@@ -9,8 +18,13 @@ function copyEvents () {
     },
   });
 
-  headerlinkClipboard.on("success", () => {
-    showSnackbar(_("Copied link to clipboard!"));
+  headerlinkClipboard.on("success", showSuccessTooltip);
+
+  // we don't want the default event, since we're using it like a button
+  document.querySelectorAll(".headerlink").forEach((link) => {
+    link.onclick = (event) => {
+      event.preventDefault();
+    }
   });
 
   // Use clipboard.js for the code copy buttons. Select the parent node.
@@ -20,9 +34,7 @@ function copyEvents () {
     },
   });
 
-  codeClipboard.on("success", () => {
-    showSnackbar(_("Copied code to clipboard!"));
-  });
+  codeClipboard.on("success", showSuccessTooltip);
 }
 
 export { copyEvents }
