8a68a932b342e4c6f76e4a08a5202c2913af1509
Kai Welke <kai687@pm.me>
2020-11-05 17:58:10 +0100

    fix: admonition IDs (#197)
    Only add IDs (automatically) to admonitions, that don't already
    have them, e.g. via explicit labels. Added unit test to verify this behavior

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,12 +7,13 @@ import pytest
 from sphinx.testing.path import path
 
 pytest_plugins = "sphinx.testing.fixtures"
+collect_ignore = ["roots"]
 
 
 @pytest.fixture(scope="session")
 def rootdir() -> Path:
     """Root directory for test files."""
-    return path(__file__).parent.abspath()
+    return path(__file__).parent.abspath() / "roots"
 
 
 def pytest_configure(config: Config) -> None:
