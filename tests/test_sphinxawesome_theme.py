fd398459233f2cff417510afb1540816d0c8ab6c
Kai Welke <kai687@pm.me>
2020-11-08 12:35:49 +0100

    chore: prepare new release (#208)

--- tests/test_sphinxawesome_theme.py
+++ tests/test_sphinxawesome_theme.py
@@ -10,7 +10,7 @@ import sphinxawesome_theme
 
 def test_returns_version() -> None:
     """It has the correct version."""
-    assert sphinxawesome_theme.__version__ == "1.16.0"
+    assert sphinxawesome_theme.__version__ == "1.16.1"
 
 
 @pytest.mark.sphinx("dummy")
