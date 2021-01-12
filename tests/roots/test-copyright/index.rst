9fd03f1af38c0702e840c257cb7d56e95905a318
Kai Welke <kai687@pm.me>
2021-01-12 10:51:19 +0100

    fix: prevent empty copyright (#285)

---
+++ tests/roots/test-copyright/index.rst
@@ -0,0 +1,5 @@
+Test
+====
+
+The content of this file is not important.
+For this test, we only check the footer.
---
+++ tests/test_copyright.py
@@ -0,0 +1,72 @@
+"""Tests for copyright info.
+
+When the `copyright` setting is empty,
+no copyright info should be printed.
+
+Issue #285.
+"""
+import re
+
+from bs4 import BeautifulSoup
+import pytest
+from sphinx.application import Sphinx
+
+
+def parse_html(filename: str) -> BeautifulSoup:
+    """Parse an HTML file into a BeautifulSoup tree."""
+    with open(filename) as file_handle:
+        tree = BeautifulSoup(file_handle, "html.parser")
+    return tree
+
+
+@pytest.mark.sphinx(
+    "html",
+    testroot="copyright",
+)
+def test_empty_copyright_info(app: Sphinx) -> None:
+    """It does not contain any Copyright info in the footer."""
+    app.build()
+    tree = parse_html(app.outdir / "index.html")
+    footer = tree("footer", role="contentinfo")
+    assert len(footer) == 1
+
+    # search for text in the children of footer
+    for child in footer:
+        matches = child.find_all(text=re.compile("Copyright"))
+        assert len(matches) == 0
+
+
+@pytest.mark.sphinx(
+    "html",
+    testroot="copyright",
+    confoverrides={"show_copyright": "False"},
+)
+def test_dont_show_copyright(app: Sphinx) -> None:
+    """It contains any Copyright info in the footer."""
+    app.build()
+    tree = parse_html(app.outdir / "index.html")
+    footer = tree("footer", role="contentinfo")
+    assert len(footer) == 1
+
+    # search for text in the children of footer
+    for child in footer:
+        matches = child.find_all(text=re.compile("Copyright"))
+        assert len(matches) == 0
+
+
+@pytest.mark.sphinx(
+    "html",
+    testroot="copyright",
+    confoverrides={"copyright": "Test"},
+)
+def test_show_copyright(app: Sphinx) -> None:
+    """It contains any Copyright info in the footer."""
+    app.build()
+    tree = parse_html(app.outdir / "index.html")
+    footer = tree("footer", role="contentinfo")
+    assert len(footer) == 1
+
+    # search for text in the children of footer
+    for child in footer:
+        matches = child.find_all(text=re.compile("Copyright"))
+        assert len(matches) == 1
