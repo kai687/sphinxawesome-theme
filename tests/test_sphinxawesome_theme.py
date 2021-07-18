fbc3a1bddc975de6778508c96d4bc2c223d21f91
Kai Welke <kai687@pm.me>
2021-07-18 17:52:35 +0200

    chore: bump version

--- tests/test_sphinxawesome_theme.py
+++ tests/test_sphinxawesome_theme.py
@@ -10,7 +10,7 @@ import sphinxawesome_theme
 
 def test_returns_version() -> None:
     """It has the correct version."""
-    assert sphinxawesome_theme.__version__ == "2.0.0"
+    assert sphinxawesome_theme.__version__ == "2.0.1"
 
 
 @pytest.mark.sphinx("dummy")
