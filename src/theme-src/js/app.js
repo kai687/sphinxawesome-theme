7f88a89a41bc08722e4c75308809ef598568786f
Kai Welke <kai687@pm.me>
2021-07-28 08:27:23 +0200

    fix: better font handling
    The fonts are in an npm package instead of manually copied.
    This makes changing and updating the fonts a lot easier.
    I also swapped `Roboto Mono` with `JetBrains Mono` which I think
    looks fabulous.

--- src/theme-src/js/app.js
+++ src/theme-src/js/app.js
@@ -1,4 +1,13 @@
 import "../css/styles.css";
+import "@fontsource/roboto/latin-400.css";
+import "@fontsource/roboto/latin-400-italic.css";
+import "@fontsource/roboto/latin-500.css";
+import "@fontsource/roboto/latin-500-italic.css";
+import "@fontsource/jetbrains-mono/latin-400.css";
+import "@fontsource/jetbrains-mono/latin-400-italic.css";
+import "@fontsource/jetbrains-mono/latin-500.css";
+import "@fontsource/jetbrains-mono/latin-500-italic.css";
+
 import { collapsibleNav } from "./nav";
 import { toggleSidebar } from "./sidebar";
 import { searchPane, clearSearchHighlights, searchEvents } from "./search";
