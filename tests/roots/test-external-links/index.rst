ca34ae6034e5571fec2de1c25d42831ea4d31ba8
Kai Welke <kai687@pm.me>
2022-05-29 10:00:01 +0200

    feat(tests): restart testing
    A few good tests are better than no tests.

--- tests/roots/test-external-links/index.rst
+++
@@ -1,7 +0,0 @@
-.. _foo:
-
-Test
-====
-
-This here should be an internal link :ref:`foo`.
-This here should be an external link `example <https://example.com>`_.
--- tests/test_sphinxawesome_theme.py
+++ tests/test_sphinxawesome_theme.py
@@ -1,7 +1,4 @@
 """Unit tests for the sphinxawesome_theme python extensions."""
-
-import os
-
 import pytest
 from sphinx.application import Sphinx
 
@@ -13,21 +10,52 @@ def test_returns_version() -> None:
     assert sphinxawesome_theme.__version__ == "3.3.0"
 
 
-@pytest.mark.sphinx("dummy")
-def test_can_access_and_compile_test(app: Sphinx) -> None:
-    """It compiles the basic test files."""
-    if app.builder is not None:
-        app.builder.build_all()
-    assert os.path.exists(app.outdir)
-    assert not os.listdir(app.outdir)
+@pytest.mark.sphinx("html")
+def test_can_compile_default_test(app: Sphinx) -> None:
+    """It can compile a basic test and doesn't load any extensions by default."""
+    app.build()
+    assert app.config.html_theme == "alabaster"
+    assert "sphinxawesome_theme" not in app.extensions
+    assert "sphinxawesome_theme.html_translator" not in app.extensions
+    assert "sphinxawesome_theme.jinja_functions" not in app.extensions
+    assert "sphinxawesome_theme.postprocess" not in app.extensions
+    assert "sphinxawesome_theme.highlighting" not in app.extensions
+    assert "sphinxawesome_theme.docsearch" not in app.extensions
 
 
-def test_loads_extensions(app: Sphinx) -> None:
-    """It loads all internal extensions."""
-    _ = sphinxawesome_theme.setup(app)
-    assert "sphinxawesome.sampdirective" in app.extensions
-    assert "sphinxawesome_theme.html_translator" in app.extensions
-    assert "sphinxawesome_theme.jinja_filters" in app.extensions
-    assert "sphinxawesome_theme.postprocess" in app.extensions
-    assert "sphinxawesome_theme.admonition_ids" in app.extensions
+@pytest.mark.sphinx(
+    "html",
+    confoverrides={
+        "html_theme": "sphinxawesome_theme",
+        "extensions": ["sphinxawesome_theme"],
+    },
+)
+def test_loads_default_extensions(app: Sphinx) -> None:
+    """It loads the project as theme and loads all extensions."""
+    app.build()
+    assert app.config.html_theme == "sphinxawesome_theme"
+    assert "sphinxawesome_theme" in app.extensions
+    assert "sphinxawesome_theme.jinja_functions" in app.extensions
     assert "sphinxawesome_theme.highlighting" in app.extensions
+    assert "sphinxawesome_theme.html_translator" in app.extensions
+    assert "sphinxawesome_theme.postprocess" in app.extensions
+    assert "sphinxawesome_theme.docsearch" in app.extensions
+
+
+@pytest.mark.sphinx(
+    "html",
+    confoverrides={
+        "html_theme": "sphinxawesome_theme",
+        "extensions": ["sphinxawesome_theme"],
+    },
+)
+def test_has_default_configuration(app: Sphinx) -> None:
+    """It uses default configuration settings."""
+    app.build()
+    assert app.config.html_awesome_headerlinks is True
+    assert app.config.html_awesome_code_headers is True
+    assert app.config.html_awesome_postprocessing is True
+    assert app.config.html_awesome_html_translator is True
+    assert app.config.html_awesome_docsearch is False
+    assert app.config.html_collapsible_definitions is False
+    assert app.config.docsearch_config == {}
