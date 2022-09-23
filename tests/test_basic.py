779bf2cf680192b8338a0ab176968376c57c33bf
Kai Welke <kai687@pm.me>
2022-09-23 10:47:06 +0200

    chore: version update

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -11,7 +11,7 @@ import sphinxawesome_theme
 
 def test_returns_version() -> None:
     """It has the correct version."""
-    assert sphinxawesome_theme.__version__ == "3.3.5"
+    assert sphinxawesome_theme.__version__ == "3.3.6"
 
 
 @pytest.mark.sphinx("dummy")
