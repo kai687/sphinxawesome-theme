60e5105af8ea0dbd52acdea17c803402cf671c52
Kai Welke <kai687@pm.me>
2023-05-03 21:20:52 +0200

    somewhat scrollspy

--- src/theme-src/js/app.js
+++ src/theme-src/js/app.js
@@ -7,11 +7,15 @@ import '@fontsource/jetbrains-mono/latin-700.css'
 import '@fontsource/jetbrains-mono/latin-700-italic.css'
 
 import Alpine from 'alpinejs'
+// import intersect from '@alpinejs/intersect'
 import { addCodeButtons } from './code'
+import { scrollSpy } from './scrollspy'
 
 window.Alpine = Alpine
+// Alpine.plugin(intersect)
 Alpine.start()
 
 window.addEventListener('DOMContentLoaded', () => {
   addCodeButtons()
+  scrollSpy()
 })
---
+++ src/theme-src/js/scrollspy.js
@@ -0,0 +1,27 @@
+/** Highlight active section in the TOC */
+export function scrollSpy() {
+  const sections = document.querySelectorAll('#content section')
+  const options = {
+    rootMargin: '-60px 0% -80% 0%',
+  }
+
+  const observer = new IntersectionObserver(highlightCurrentSection, options)
+
+  sections.forEach((section) => {
+    observer.observe(section)
+  })
+}
+
+function highlightCurrentSection(entries) {
+  entries.forEach((entry) => {
+    const matchingLink = document.querySelector(
+      `#right-sidebar a[href*=${entry.target.id}]`
+    )
+
+    if (entry.isIntersecting && matchingLink) {
+      matchingLink.setAttribute('data-current', 'true')
+    } else if (matchingLink) {
+      matchingLink.removeAttribute('data-current')
+    }
+  })
+}
