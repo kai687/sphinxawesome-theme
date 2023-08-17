0e5c962eecf2cd146ec36654fb61fe478d89a126
Kai Welke <kai687@pm.me>
2023-08-17 14:11:25 +0200

    fix: code copy button (#1465)
    Fixes the copy button target to copy only the code, not the tooltip
    text.
    Pinned the Sphinx version because of #1464.

--- src/sphinxawesome_theme/postprocess.py
+++ src/sphinxawesome_theme/postprocess.py
@@ -20,6 +20,7 @@ They might not show up in the final CSS.
 from __future__ import annotations
 
 import os
+import pathlib
 import re
 from dataclasses import dataclass
 
@@ -41,7 +42,7 @@ class Icons:
     permalinks_icon: str = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" width="1em" viewBox="0 0 24 24"><path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"/></svg>'
 
 
-def _get_html_files(outdir: str) -> list[str]:
+def _get_html_files(outdir: pathlib.Path | str) -> list[str]:
     """Get a list of HTML files."""
     html_list = []
     for root, _, files in os.walk(outdir):
