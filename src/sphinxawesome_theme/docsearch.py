kai687/sphinxawesome-theme

commit b6451a383238dea80e92d110ea2fa6f3dd4a9a8e

fix: progress_message is imported incorrectly in sphinx5 (#1538)
Use old path for `sphinx.util.progress_message` to stay compatible with Sphinx 5.

src/sphinxawesome_theme/docsearch.py
@@ -22,8 +22,7 @@ from sphinx.builders.dirhtml import DirectoryHTMLBuilder
 from sphinx.builders.html import StandaloneHTMLBuilder
 from sphinx.config import Config
 from sphinx.locale import __
-from sphinx.util import logging
-from sphinx.util.display import progress_message
+from sphinx.util import logging, progress_message
 
 from . import __version__
 

commit a071a6e1645a4c4c048bd964b4ac3bdd32df0ba3

fix: type errors (#1539)
Fix a new type error introduced by #1538.

src/sphinxawesome_theme/docsearch.py
@@ -97,7 +97,7 @@ class DocSearchConfig:
     """
 
 
-@progress_message("DocSearch: check config")
+@progress_message("DocSearch: check config")  # type: ignore
 def check_config(app: Sphinx, config: Config) -> None:
     """Set up Algolia DocSearch.
 
@@ -121,7 +121,7 @@ def check_config(app: Sphinx, config: Config) -> None:
         )
 
 
-@progress_message("DocSearch: add assets")
+@progress_message("DocSearch: add assets")  # type: ignore
 def add_docsearch_assets(app: Sphinx, config: Config) -> None:
     """Add the docsearch.css file for DocSearch."""
     app.add_css_file("docsearch.css", priority=150)