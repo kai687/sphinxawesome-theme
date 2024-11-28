1a597e33e024dc32c880e6d03aa9f02087eb30be
Kai Welke <kai687@pm.me>
2024-11-28 22:59:47 +0100

    Even more unused definitions

--- src/theme-src/tailwind.config.js
+++ src/theme-src/tailwind.config.js
@@ -21,14 +21,7 @@ module.exports = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
+        primary: "hsl(var(--primary))",
         muted: {
           DEFAULT: "hsl(var(--muted))",
           foreground: "hsl(var(--muted-foreground))",
