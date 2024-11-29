1edc29432237725b9517727c048819a76e0cc7a3
Kai Welke <kai687@pm.me>
2024-11-29 20:28:22 +0100

    Need postcss-custom-selectors (#2086)
    Removed `bg-opacity` classes that didn't have an effect anyway and add
    the `postcss-custom-selectors` plugin for the headerlinks. Some settings
    I changed must have automatically taken care of it before.

--- src/theme-src/postcss.config.js
+++ src/theme-src/postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
+    require("postcss-custom-selectors"),
     require("postcss-preset-env")({
       minimumVendorImplementations: 2,
     }),
