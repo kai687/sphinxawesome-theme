0c8240456f9391cfab7bf7e9941130ae757ade67
Kai Welke <kai687@pm.me>
2026-01-21 19:57:12 +0100

    chore: types

--- tests/util.py
+++ tests/util.py
@@ -1,19 +1,18 @@
 """Test utility functions."""
 
 from pathlib import Path
-from typing import Union
 
 from bs4 import BeautifulSoup
 
 
-def parse_html(filename: Union[Path, str]) -> BeautifulSoup:
+def parse_html(filename: Path | str) -> BeautifulSoup:
     """Parse an HTML file into a BeautifulSoup tree."""
     with open(filename) as file_handle:
         tree = BeautifulSoup(file_handle, "html.parser")
     return tree
 
 
-def read_as_text(filename: Union[Path, str]) -> str:
+def read_as_text(filename: Path | str) -> str:
     """Read a file as text."""
     with open(filename) as file_handle:
         txt = file_handle.read()
